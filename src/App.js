99e5c3226c075fd1e0c40aa5346cb079df4a5273
chenchuhua <3361694095@qq.com>
2022-01-08 08:34:06 +0800

    删除不必要文件

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+/*
+ * @Description: 
+ * @version: 
+ * @Author: simpletoyou
+ * @Date: 2022-01-07 16:47:41
+ * @LastEditors: simpletoyou
+ * @LastEditTime: 2022-01-08 08:32:32
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      APP
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: simpletoyou
+ * @Date: 2022-01-07 16:47:41
+ * @LastEditors: simpletoyou
+ * @LastEditTime: 2022-01-08 08:32:08
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
