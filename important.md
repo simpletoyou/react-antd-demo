3f11cf36ee457af536f6339a9221c94ac82c405c
chenchuhua <3361694095@qq.com>
2022-01-08 17:15:20 +0800

    update

---
+++ important.md
@@ -0,0 +1,11 @@
+<!--
+ * @Description: 
+ * @version: 
+ * @Author: simpletoyou
+ * @Date: 2022-01-08 16:24:57
+ * @LastEditors: simpletoyou
+ * @LastEditTime: 2022-01-08 17:05:32
+-->
+
+
+        
--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@
  * @Author: simpletoyou
  * @Date: 2022-01-07 16:47:41
  * @LastEditors: simpletoyou
- * @LastEditTime: 2022-01-08 11:25:14
+ * @LastEditTime: 2022-01-08 14:28:39
  */
 
 import React from 'react';
 // babel-plugin-import 会帮助你加载JS和CSS
-// import { DatePicker } from "antd";
+import { Button, Pagination } from "antd";
 
 
-import Button from 'antd/es/button'
-import 'antd/es/button/style/css'
+// import Button from 'antd/es/button'
+// import 'antd/es/button/style/css'
 
 
 
 function App() {
+
+  function pageChange(page,pageSize) {
+    console.log(page,pageSize)
+  }
   return (
     <div className="App">
-      APP
       <Button type="primary">Primary Button</Button>
       <Button>Default Button</Button>
       <Button type="dashed">Dashed Button</Button>
@@ -34,6 +37,7 @@ function App() {
         <DatePicker onChange={onChange} picker="quarter" />
         <DatePicker onChange={onChange} picker="year" />
       </Space> */}
+      <Pagination showQuickJumper defaultCurrent={1} total={50} onChange={pageChange}/>
     </div>
   );
 }
